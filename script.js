0d50ee0edec7869da2730d6c2e88b9a5353a05d5
kale <kamarakale4@gmail.com>
2022-10-12 16:00:12 -0500

    Add dataset to html file

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-// Basic math functions
-const add = (a,b)=> a+b;
-const subtract = (a,b) => a -b;
-const multiply = (a,b) => a*b;
-const divide = (a,b) => a/b;
+const keys = document.querySelector('.grid-container');
+const display = document.querySelector('.screen');
 
-// Takes 2 numbers and a sign, and returns result
-function operate(num1, sign, num2){
-    if(sign == '+'){
-        return add(num1,num2)   
-    }else if(sign == '-'){
-        return subtract(num1, num2) 
-    }else if(sign == '*'){
-        return multiply(num1,num2)
-    }else if(sign == '/'){
-        return divide(num1,num2)
+
+keys.addEventListener('click', event =>{
+  
+  if(!event.target.closest('button')) return
+
+  const key = event.target
+  const keyValue = key.textContent
+  const displayValue = display.textContent
+  const type = key.dataset.type
+  const previousKey = keys.dataset.previousKeyType
+
+  
+  // Check if its a number
+  if (key.classList.contains('number')){
+    if(displayValue ==='0'){
+    display.textContent = keyValue
+    }else if (previousKey == 'operator'){
+      display.textContent = keyValue
+    }else{
+      display.textContent = displayValue + keyValue
     }
+  }
 
-}
+  // Check if its a operator key
+  if(type === 'operator'){
+    console.log(key)
+    
 
+   
+  }
+    
+  if(key.classList.contains('clear')){
+    location.reload();
+  }
+   
+  keys.dataset.previousKeyType = type;
+   
+});
